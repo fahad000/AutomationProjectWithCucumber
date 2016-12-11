d23b5325aeb99b9d160b2d1711c3aabdf2634c1b
fahad000 <fahadk0938@gmail.com>
2016-12-11 14:32:41 -0500

    Changed the printout message

--- src/test/java/com/cbt/DemoTest.java
+++ src/test/java/com/cbt/DemoTest.java
@@ -3,7 +3,7 @@ package com.cbt;
 public class DemoTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
---
+++ src/test/resources/Hook.java
@@ -0,0 +1,6 @@
+
+public class Hook {
+public static void main(String[] args) {
+	System.out.println("this is message from hook class");
+}
+}
